4224d2cdf2b429fbb788aaa5c0c1e1f19e71a27c
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-21 17:36:02 -0300

    Agregando Fuentes de Googe Fonts y Más

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import { Global, css } from '@emotion/core';
+import Head from 'next/head';
 
 
 const Layout = props => {
@@ -24,10 +25,18 @@ const Layout = props => {
                     body {
                         font-size: 1.6rem; /** 16PX */
                         line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
                     }
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
                     }
                     ul {
                         list-style: none;
@@ -42,6 +51,15 @@ const Layout = props => {
                 `}
             
             />
+
+            <Head>
+                <html lang="es" />
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet" />
+                <link href="/static/css/app.css" rel="stylesheet" />
+
+            </Head>
             <Header />
 
             <main>
