027fe519d813b28bfaba4fe9baa22b3de91b4751
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-21 16:37:29 -0300

    Routing en Next.js

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout/Layout'
+
+
+
+const Nosotros = () => (
+  <div>
+      <Layout>
+        <h1>Nosotros</h1>
+      </Layout>
+  </div>
+)
+
+
+export default Nosotros
