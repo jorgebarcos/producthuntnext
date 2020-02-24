0250c894ca4bb25ca31501b541d9455ec7f063a8
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-24 07:29:37 -0300

    Creando el Formulario para Crear Cuentas

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,8 +5,47 @@ import Layout from '../components/layout/Layout'
 const CrearCuenta = () => (
   <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1>Crear Cuenta</h1>
+          <form>
+            <div>
+              <lable htmlFor="nombre">Nombre</lable>
+              <input 
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+
+              />
+            </div>
+            <div>
+              <lable htmlFor="email">Email</lable>
+              <input 
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+                
+              />
+            </div>
+            <div>
+              <lable htmlFor="nombre">Password</lable>
+              <input 
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+                
+              />
+            </div>
+            <input 
+              type="submit"
+              value="Crear Cuenta"
+            />
+          </form>
+        </>
       </Layout>
+
   </div>
 )
 
