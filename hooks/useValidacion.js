62d8c6357dd95ebd282f092de17faef14a314f83
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-24 08:25:34 -0300

    Finalizando el Hook

--- hooks/useValidacion.js
+++ hooks/useValidacion.js
@@ -10,10 +10,36 @@ const useValidacion = (stateInicial, validar, fn) => {
         if(submitForm) {
             const noErrores = Object.keys(errores).length === 0;
         }
+
+            if(noErrores) {
+                fn(); // Fn= Función que se ejecuta en el componente
+            }
+            guardarSubmitForm(false);
     }, []);
 
-    return ( 
+    // Función que se ejecuta conforme el usuario escribe algo
+    const handleChange = e => {
+        guardarValores({
+                ...valores,
+                [e.target.name] : e.target.value
+            
+        })
+    }
 
+    // función que se ejecuta cuando el usuario hace submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        const erroresValidacion = validar(valores);
+        guardarErrores(erroresValidacion);
+        guardarSubmitForm(true);
+    }
+
+    return ( 
+        valores,
+        errores,
+        submitForm,
+        handleSubmit,
+        handleChange
      );
 }
  
