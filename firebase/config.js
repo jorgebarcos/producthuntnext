7a645d706338a5b04e78d97eae9f552577104065
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-07-14 19:49:25 -0300

    Creando los archivos de configuración de Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgMKhdTgfPIEPAStmhUCcqBzRRfeGfAes",
+    authDomain: "product-hunt-f1866.firebaseapp.com",
+    databaseURL: "https://product-hunt-f1866.firebaseio.com",
+    projectId: "product-hunt-f1866",
+    storageBucket: "product-hunt-f1866.appspot.com",
+    messagingSenderId: "710638557320",
+    appId: "1:710638557320:web:8a10f2d86294077073e73d"
+};
+
+export default firebaseConfig;
