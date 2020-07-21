00ef733ba0e73b926ebd11c32774191dd6939013
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-07-21 10:26:53 -0300

    Agregando Validación

---
+++ validacion/validarCrearProducto.js
@@ -0,0 +1,28 @@
+export default function validarCrearProducto(valores) {
+
+    let errores = {};
+
+    // Validar el nombre del usuario}
+    if(!valores.nombre) {
+        errores.nombre = "El Nombre es obligatorio";
+    }
+
+    // validar el empresa
+    if(!valores.empresa) {
+        errores.empresa = "Nombre de Empresa es obligatorio"
+    }
+    
+    // validar el url
+    if(!valores.url) {
+        errores.url = "La URL del producto es obligatoria"
+    } else if(!/^(ftp|http|https):\/\/[^ "]+$/.test(valores.url)) {
+        errores.url = "URL mal formateada o no válida"
+    }
+
+    // Validar descripción
+    if(!valores.descripcion) {
+        errores.descripcion = "Agrega una descripción de tu producto"
+    }
+    
+    return errores;
+}
